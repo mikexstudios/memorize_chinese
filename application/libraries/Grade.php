98f80ecf108b25046f7926439a5eb0404ae178e6
Michael Huynh <mike.huynh@gmail.com>
2007-12-01 09:57:33 +0000

    Just completed the Grading side of the new algorithm. Most likely doesn't work yet.

--- application/libraries/Grade.php
+++ application/libraries/Grade.php
@@ -3,24 +3,115 @@
 class Grade {
 	var $CI;
 	
+	var $card_id;
+	var	$new_grade;
+	
 	function Grade() {
 		$this->CI =& get_instance();
 	}
 	
-	function get_previous($in_id) {
-		$previous_grade = $this->CI->user_progress_model->get_answer_rating($in_id);
-		if(empty($previous_grade))
-		{
-			$previous_grade = 2.5;
-		}
-		$previous_grade = floatval($previous_grade);	
+	function set_new_grade($in_new_grade) {
+		$this->new_grade = $in_new_grade;
 		
-		return $previous_grade;
+		//Now we run through algorithm for grading:
+		$this->CI->user_progress_model->card_id = $this->new_grade;
+		$previous_grade = $this->CI->user_progress_model->get_answer_rating();
+		if ($previous_grade === false) //We never encountered this card yet.
+		{
+			$previous_grade = 0; //Assign initial grade of zero. This will be adjusted by new grade.
+		}
+		
+		//If the user gets two 5's in a row, then we assume that the card has been learnt.
+		if($previous_grade >= 5 && $this->new_grade >= 5)
+		{
+			//We bump the inverval up
+			$this->CI->user_progress_model->increment_interval();
+			
+			//Compute next repetition date
+			$this->CI->set_next_repetition_date();
+		}
+	}
+
+//--------------------------------------------------------------------------------------------------
+
+	function set_next_repetition_date() {
+		$interval = intval($this->CI->user_progress_model->get_interval());
+		$repetitions_to_memorize = intval($this->CI->user_progress_model->get_repetitions());
+		
+		$next_repetition_days = 0; //Initialize
+		switch($interval)
+		{
+			case 1:
+				$next_repetition_days = $this->_interval_1($repetitions_to_memorize);
+			case 2:
+				$next_repetition_days =  $this->_interval_2($repetitions_to_memorize);
+			default:
+				if($interval <= 0) { return false; }
+				$next_repetition_days = $this->_interval_gteq_3($interval, $repetitions_to_memorize);
+		}
+		
+		//Adjust current date (now) with days computed
+		//Days * 24 hours * 60 minutes * 60 seconds. We want to convert days to seconds.
+		$next_repetition_date = now() + ($next_repetition_days * 24 * 60 * 60); 
+		$this->CI->user_progress_model->set_next_repetition_date($next_repetition_date);
 	}
 	
-	function get_new($previous_grade, $in_grade) {
-		return $in_grade;	
+	function _interval_1($repetitions) {
+		switch($repetitions)
+		{
+			case 1:
+				return 4;
+			case 2:
+				return 3;
+			case 3:
+				return 2;
+			default:
+				if($interval <= 0) {return false;}
+				return 1;
+		}
 	}
+
+	function _interval_2($repetitions) {
+		switch($repetitions)
+		{
+			case 1:
+				return 8;
+			case 2:
+				return 7;
+			case 3:
+				return 6;
+			case 4:
+				return 5;
+			default:
+				if($interval <= 0) {return false;}
+				return 1;
+		}	
+	}
+
+	function _interval_gteq_3($interval, $repetitions) {
+		if($interval <= 3)
+		{
+			$days = 16;
+		}
+		else
+		{
+			$days = 35 * pow(2, ($interval-4));
+		}
+		
+		switch($repetitions)
+		{
+			case 1:
+				return $days+1;
+			case 2:
+				return $days;
+			case 3:
+				return floor(floatval($days)*0.94);
+			default:
+				if($interval <= 0) {return false;}
+				return floor(floatval($days)*0.44)-($repetitions-7);
+		}		
+	}
+	
 
 }
 
--- application/models/user_progress_model.php
+++ application/models/user_progress_model.php
@@ -2,6 +2,7 @@
 
 class User_progress_model extends Model {
 	
+	var $card_id;
 	//var $id, $category, $question, $answer;
 	var $flipped = false; //Defines whether or not we are in the flipped card state.
 	
@@ -27,39 +28,115 @@ class User_progress_model extends Model {
 		$this->where_flipped_check();
 		$this->db->limit(1);
 		
-		$query = $this->db->get();
-		return $query->row_array();
+		if($this->db->affected_rows() >  0) 
+		{
+			$query = $this->db->get();
+			return $query->row_array();
+		}
+		
+		return false;
 	}
 	
-	function get_answer_rating($card_id) {
-		$temp = $this->get_card_id_one($card_id);
+	function get_answer_rating() {
+		$temp = $this->get_card_id_one();
 		
 		if(!isset($temp['answer_rating']))
 		{
 			return false;
 		}
 		
-		return $temp['answer_rating'];
+		return floatval($temp['answer_rating']);
+	}
+
+	function get_interval() {
+		$temp = $this->get_card_id_one();
+		return $temp['interval'];
 	}
 	
-	function set($card_id, $answer_rating, $date_answered, $time_to_answer) {
+	function get_repetitions() {
+		$temp = $this->get_card_id_one();
+		return $temp['repetitions_to_memorize'];
+	}
+	
+	function set($answer_rating, $date_answered, $interval, $repetitions_to_memorize) {
 		//Check to see if user has already encountered card.
-		$temp = $this->get_answer_rating($card_id);
+		$temp = $this->get_answer_rating();
 		//die($answer_rating);
 		$this->db->set('answer_rating', $answer_rating);
 		$this->db->set('date_answered', $date_answered);
-		$this->db->set('time_to_answer', $time_to_answer);
+		$this->db->set('interval', $interval);
+		$this->db->set('repetitions_to_memorize', $repetitions_to_memorize);
 		$this->db->limit(1);
 		
 		if($temp !== false)
 		{
-			$this->db->where('card_id', $card_id);
+			$this->db->where('card_id', $this->card_id);
 			$this->where_flipped_check();
 			$this->db->update(USER_PROGRESS_TABLE);
 		}
 		else
 		{
-			$this->db->set('card_id', $card_id);
+			$this->db->set('card_id', $this->card_id);
+			if($this->flipped === true)
+			{
+				$this->db->set('flipped', 1);
+			}
+			
+			$this->db->insert(USER_PROGRESS_TABLE);
+		}
+	}
+	
+	function increment_interval($card_id) {
+		if($this->get_card_id_one() !== false)
+		{
+			$old_interval = $this->get_interval();
+			$this->set_interval($old_interval+1);
+			return true;
+		}
+		
+		return false;	
+	}
+
+	function set_interval($in_interval) {
+		$temp = $this->get_interval();
+		
+		$this->db->set('interval', $in_interval);
+		if($temp !== false)
+		{
+			$this->db->where('card_id', $this->card_id);
+			$this->where_flipped_check();
+			$this->db->update(USER_PROGRESS_TABLE);
+		}
+		else
+		{
+			$this->db->set('card_id', $this->card_id);
+			if($this->flipped === true)
+			{
+				$this->db->set('flipped', 1);
+			}
+			
+			$this->db->insert(USER_PROGRESS_TABLE);
+		}
+	}
+	
+	function get_next_repetition_date() {
+		$temp = $this->get_card_id_one();
+		return $temp['next_repetition_date'];
+	}
+	
+	function set_next_repetition_date($in_date) {
+		$temp = $this->get_next_repetition_date();
+		
+		$this->db->set('next_repetition_date', $in_date);
+		if($temp !== false)
+		{
+			$this->db->where('card_id', $this->card_id);
+			$this->where_flipped_check();
+			$this->db->update(USER_PROGRESS_TABLE);
+		}
+		else
+		{
+			$this->db->set('card_id', $this->card_id);
 			if($this->flipped === true)
 			{
 				$this->db->set('flipped', 1);
