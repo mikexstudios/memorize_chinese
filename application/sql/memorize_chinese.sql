22edce7249993e027d1e496dd6ff4ffb7b80ff6e
Michael Huynh <mike.huynh@gmail.com>
2007-12-09 08:24:20 +0000

    Adding lessons up to 09 and updated sql schema.

--- application/sql/memorize_chinese.sql
+++ application/sql/memorize_chinese.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 24, 2007 at 08:57 PM
+-- Generation Time: Dec 09, 2007 at 12:23 AM
 -- Server version: 5.0.45
 -- PHP Version: 5.2.4
 
@@ -27,7 +27,7 @@ CREATE TABLE `cards` (
   `answer` varchar(400) NOT NULL,
   `extra` text NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=243 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=446 ;
 
 --
 -- Dumping data for table `cards`
@@ -234,72 +234,120 @@ INSERT INTO `cards` (`id`, `deck_id`, `question`, `answer`, `extra`) VALUES
 (198, 6, '通', 'tōng', '(通过) go through; 交通 traffic  [tòng] (measure word)'),
 (199, 6, '德', 'dé', 'virtue; 道德 dàodé ethics; 德国 Germany (Deutchland)'),
 (200, 6, '森', 'sēn', '森林 sēnlín forest'),
-(201, 7, '事', 'shì', 'thing, event; 事情 shìqing situation'),
-(202, 7, '闹', 'nào', '(热闹) noisy; 闹钟 alarm clock'),
-(203, 7, '翻', 'fān', 'turn over; 翻译 fānyì translate'),
-(204, 7, '详', 'xiáng', '详细 detailed; 端详 details; 安详 serene'),
-(205, 7, '细', 'xì', 'thin; 仔细 zǐxì careful; 细胞 xìbāo cell; 详细 detailed'),
-(206, 7, '况', 'kuàng', '情况 circumstances; 况且 moreover'),
-(207, 7, '性', 'xìng', 'sex; nature; -ness'),
-(208, 7, '格', 'gé', 'check, pattern, standard; 格外 géwài especially  [gē]'),
-(209, 7, '朗', 'lǎng', '爽朗 shuǎnglǎng, 明朗 bright & clear; 朗讼 recite'),
-(210, 7, '脾', 'pí', 'spleen; 脾气 píqi (bad-) temper'),
-(211, 7, '急', 'jí', 'urgent; 着急 zháojí anxious; 急忙 jímáng in a hurry'),
-(212, 7, '躁', 'zào', 'rash, impetuous'),
-(213, 7, '趣', 'qù', 'interest, fun; 兴趣 interest; 趣味 interest; taste'),
-(214, 7, '戏', 'xì', '(戏剧 xìjù) drama, play; 游戏 yóuxì game'),
-(215, 7, '古', 'gǔ', '(古代 gǔdài) ancient'),
-(216, 7, '典', 'diǎn', '典型 diǎnxíng typical; 古典 classical; 词典 dictionary'),
-(217, 7, '摇', 'yáo', 'shake'),
-(218, 7, '滚', 'gǔn', 'roll; boil'),
-(219, 7, '往', 'wǎng', 'to, toward; 往往 wǎngwǎng often'),
-(220, 7, '相', 'xiāng', 'each other, mutual  [xiàng] look at; appearance; photo'),
-(221, 7, '背', 'bèi', 'back  [bēi] carry on the back'),
-(222, 7, '景', 'jǐng', 'scene, scenery'),
-(223, 7, '噢', 'ō', 'Oh (interjection)'),
-(224, 7, '醉', 'zuì', 'drunk'),
-(225, 7, '厉', 'lì', '厉害 terrible; formidable  [lài] (name of ancient country)'),
-(226, 7, '害', 'hài', 'harmful; 害虫 hàichóng destructive insect; 厉害 lìhai fierce'),
-(227, 7, '镜', 'jìng', '(镜子) mirror; lens; 眼镜 yǎnjìng glasses'),
-(228, 7, '破', 'pò', 'smash; 破坏 pòhuài ruin; 破烂 pòlàn worn out'),
-(229, 7, '吹', 'chuī', 'blow'),
-(230, 7, '怪', 'guài', 'blame; (奇怪 qíguài) strange; 怪物 guàiwu monster'),
-(231, 7, '挺', 'tǐng', 'quite, very; endure, stand; 挺立 stand upright'),
-(232, 7, '悔', 'huǐ', '(后悔 hòuhuǐ) regret, repent'),
-(233, 7, '假', 'jiǎ', 'false  [jià] (假期 jiàqī) vacation'),
-(234, 7, '憋', 'biē', 'check, restrain, suppress; suffocate, feel depressed'),
-(235, 7, '死', 'sǐ', 'die, death'),
-(236, 7, '演', 'yǎn', '(表演 biǎoyǎn) perform, act, performance'),
-(237, 7, '部', 'bù', '(部分 bùfen) part; 西部 western part; 全部 entire'),
-(238, 7, '谈', 'tán', 'talk'),
-(239, 7, '华', 'huá', 'magnificent; China    [Huà] (surname); 华山'),
-(240, 7, '姆', 'mǔ', 'nurse; 汤姆 Tāngmǔ Tom'),
-(241, 7, '丽', 'lì', '美丽 beautiful; 壮丽 majestic  [Lí] Korea'),
-(242, 7, '莎', 'shā', '(used in names)  [suō] 莎草 (a kind of plant)');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cards_test`
---
-
-DROP TABLE IF EXISTS `cards_test`;
-CREATE TABLE `cards_test` (
-  `id` int(100) NOT NULL auto_increment,
-  `category` varchar(300) NOT NULL,
-  `question` varchar(300) NOT NULL,
-  `answer` varchar(400) NOT NULL,
-  `extra` text NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `cards_test`
---
-
-INSERT INTO `cards_test` (`id`, `category`, `question`, `answer`, `extra`) VALUES
-(1, 'Lesson 4', '我', 'wo3', ''),
-(2, 'Lesson 4', '他', 'ta1', '');
+(393, 7, '醉', 'zuì', 'drunk'),
+(392, 7, '噢', 'ō', 'Oh (interjection)'),
+(391, 7, '景', 'jǐng', 'scene, scenery'),
+(390, 7, '背', 'bèi', 'back  [bēi] carry on the back'),
+(389, 7, '相', 'xiāng', 'each other, mutual  [xiàng] look at; appearance; photo'),
+(388, 7, '往', 'wǎng', 'to, toward; 往往 wǎngwǎng often'),
+(386, 7, '摇', 'yáo', 'shake'),
+(387, 7, '滚', 'gǔn', 'roll; boil'),
+(385, 7, '典', 'diǎn', '典型 diǎnxíng typical; 古典 classical; 词典 dictionary'),
+(384, 7, '古', 'gǔ', '(古代 gǔdài) ancient'),
+(383, 7, '戏', 'xì', '(戏剧 xìjù) drama, play; 游戏 yóuxì game'),
+(382, 7, '趣', 'qù', 'interest, fun; 兴趣 interest; 趣味 interest; taste'),
+(381, 7, '躁', 'zào', 'rash, impetuous'),
+(380, 7, '急', 'jí', 'urgent; 着急 zháojí anxious; 急忙 jímáng in a hurry'),
+(379, 7, '脾', 'pí', 'spleen; 脾气 píqi (bad-) temper'),
+(378, 7, '朗', 'lǎng', '爽朗 shuǎnglǎng, 明朗 bright & clear; 朗讼 recite'),
+(370, 7, '事', 'shì', 'thing, event; 事情 shìqing situation'),
+(371, 7, '闹', 'nào', '(热闹) noisy; 闹钟 alarm clock'),
+(372, 7, '翻', 'fān', 'turn over; 翻译 fānyì translate'),
+(373, 7, '详', 'xiáng', '详细 detailed; 端详 details; 安详 serene'),
+(374, 7, '细', 'xì', 'thin; 仔细 zǐxì careful; 细胞 xìbāo cell; 详细 detailed'),
+(375, 7, '况', 'kuàng', '情况 circumstances; 况且 moreover'),
+(376, 7, '性', 'xìng', 'sex; nature; -ness'),
+(377, 7, '格', 'gé', 'check, pattern, standard; 格外 géwài especially  [gē]'),
+(402, 7, '假', 'jiǎ', 'false  [jià] (假期 jiàqī) vacation'),
+(403, 7, '憋', 'biē', 'check, restrain, suppress; suffocate, feel depressed'),
+(401, 7, '悔', 'huǐ', '(后悔 hòuhuǐ) regret, repent'),
+(399, 7, '怪', 'guài', 'blame; (奇怪 qíguài) strange; 怪物 guàiwu monster'),
+(400, 7, '挺', 'tǐng', 'quite, very; endure, stand; 挺立 stand upright'),
+(397, 7, '破', 'pò', 'smash; 破坏 pòhuài ruin; 破烂 pòlàn worn out'),
+(398, 7, '吹', 'chuī', 'blow'),
+(396, 7, '镜', 'jìng', '(镜子) mirror; lens; 眼镜 yǎnjìng glasses'),
+(395, 7, '害', 'hài', 'harmful; 害虫 hàichóng destructive insect; 厉害 lìhai fierce'),
+(369, 8, '敢', 'gǎn', 'dare; 勇敢 yǒnggǎn brave'),
+(394, 7, '厉', 'lì', '厉害 terrible; formidable  [lài] (name of ancient country)'),
+(368, 8, '炸', 'zhà', '(爆炸 bàozhà) explode, blow up  [zhá] deep fry'),
+(367, 8, '胁', 'xié', 'upper part of side of person''s body; coerce'),
+(366, 8, '威', 'wēi', '威胁 wēixié threaten, imperil; 示威 demonstrate'),
+(365, 8, '盾', 'dùn', 'shield; 矛盾 máodùn contradiction ("spear-shield")'),
+(364, 8, '矛', 'máo', 'spear; 矛盾 máodùn contradiction ("spear-shield")'),
+(363, 8, '任', 'rèn', 'appoint; no matter; 任务 task; 任何 any  [Rén] (surname)'),
+(362, 8, '责', 'zé', 'duty; 责任 zérèn responsibility'),
+(361, 8, '负', 'fù', 'carry on shoulder; suffer; negative'),
+(360, 8, '借', 'jiè', 'borrow, lend'),
+(359, 8, '枪', 'qiāng', 'gun   [chēng] 欃枪 chánchēng comet'),
+(358, 8, '向', 'xiàng', 'towards (direction); (F曏) formerly'),
+(357, 8, '免', 'miǎn', 'exempt; 避免 bìmiǎn avoid; 免费 miǎnfèi free'),
+(356, 8, '怪', 'guài', 'blame; (奇怪 qíguài) strange; 怪物 guàiwu monster'),
+(355, 8, '糟', 'zāo', 'rotten; 糟糕 zāogāo bad luck! 糟踏 zāota waste, spoil'),
+(354, 8, '乱', 'luàn', 'chaotic, disorderly, messy; 混乱 hùnluàn chaos'),
+(353, 8, '死', 'sǐ', 'die, death'),
+(352, 8, '烧', 'shāo', 'roast, cook; burn (cf 炒 chǎo stir-fry)'),
+(351, 8, '灾', 'zāi', '(灾难 zāinàn, 灾害 zāihài) disaster'),
+(350, 8, '柴', 'chái', 'firewood; 火柴 huǒchái match; 柴油 cháiyóu diesel oil'),
+(349, 8, '模', 'mó', '模范 model; 模仿 imitate  [mú] 模样 appearance; 模子'),
+(348, 8, '据', 'jù', 'seize; depend on; (根据 gēnjù) according to  [jū] 拮据'),
+(347, 8, '引', 'yǐn', 'draw, stretch; (引起 yǐnqǐ) cause; (吸引 xīyǐn) attract'),
+(346, 8, '童', 'tóng', '儿童 értóng children'),
+(345, 8, '通', 'tōng', '(通过) go through; 交通 traffic  [tòng] (measure word)'),
+(344, 8, '闻', 'wén', 'smell; hear; 新闻 news'),
+(343, 8, '频', 'pín', '频率 pínlǜ frequency'),
+(342, 8, '闲', 'xián', 'idle; free time; 闲话 digression; complaint'),
+(341, 8, '录', 'lù', 'record; 记录 jìlù take notes; 录音机 tape recorder'),
+(340, 8, '纪', 'jì', '世纪 century; 纪念 memento; 纪律 discipline'),
+(339, 8, '尔', 'ěr', '(archaic:) you; 尔后 thereafter; 高尔夫 golf'),
+(338, 8, '偶', 'ǒu', '(偶然 ǒurán) by chance; even number (cf. 奇 jī odd)'),
+(337, 8, '术', 'shù', 'skill; (艺术 yìshù) art; 技术 technique  [zhú]'),
+(336, 8, '艺', 'yì', 'skill; (艺术 yìshù) art; 文艺 literature and art'),
+(335, 8, '堂', 'táng', 'hall; 食堂 dining hall; 课堂 classroom  [tāng] 亮堂堂'),
+(334, 8, '商', 'shāng', 'discuss; business (store)'),
+(333, 8, '演', 'yǎn', '(表演 biǎoyǎn) perform, act, performance'),
+(332, 8, '响', 'xiǎng', 'sound; 影响 yǐngxiǎng influence'),
+(404, 7, '死', 'sǐ', 'die, death'),
+(405, 7, '演', 'yǎn', '(表演 biǎoyǎn) perform, act, performance'),
+(406, 7, '部', 'bù', '(部分 bùfen) part; 西部 western part; 全部 entire'),
+(407, 7, '谈', 'tán', 'talk'),
+(408, 7, '华', 'huá', 'magnificent; China    [Huà] (surname); 华山'),
+(409, 7, '姆', 'mǔ', 'nurse; 汤姆 Tāngmǔ Tom'),
+(410, 7, '丽', 'lì', '美丽 beautiful; 壮丽 majestic  [Lí] Korea'),
+(411, 7, '莎', 'shā', '(used in names)  [suō] 莎草 (a kind of plant)'),
+(412, 9, '代', 'dài', '(代表 dàibiǎo) represent; (时代 shídài) generation'),
+(413, 9, '移', 'yí', '(移动) move; 转移 shift; transform; 移植 transplant'),
+(414, 9, '曾', 'céng', '(曾经 céngjīng) in the past  [zēng] great- (grandmother)'),
+(415, 9, '陆', 'lù', 'land; 大陆 continent   [liù] six ( = 六)'),
+(416, 9, '确', 'què', 'true; 正确 correct; 确实 indeed; 明确 explicit'),
+(417, 9, '否', 'fǒu', 'no; 是否 whether   [pǐ]  Standstill'),
+(418, 9, '则', 'zé', '(conjunction); 原则 yuánzé principle; 否则 fǒuzé or else'),
+(419, 9, '散', 'sàn', 'break up; distribute; dispel  [sǎn] fall apart; scattered'),
+(420, 9, '步', 'bù', 'step; 进步 jìnbù progress; 散步 sànbù go for a walk'),
+(421, 9, '岸', 'àn', 'shore'),
+(422, 9, '腻', 'nì', 'greasy, oily; smooth; (腻烦 nìfan) disgusted, fed up'),
+(423, 9, '炉', 'lú', '(炉子 lúzi) stove, oven, furnace'),
+(424, 9, '姑', 'gū', '姑娘 aunt'),
+(425, 9, '既', 'jì', 'since; already; 既然 jìrán since, now that'),
+(426, 9, '趟', 'tàng', '(measure word) trip; row  [tāng] ( = 蹚)'),
+(427, 9, '社', 'shè', '(社会 shèhuì) society'),
+(428, 9, '订', 'dìng', 'conclude (cf 定)'),
+(429, 9, '线', 'xiàn', 'line, thread; 光线 light ray; 电线 wire'),
+(430, 9, '量', 'liàng', 'amount; 量词 classifier  [liáng] measure'),
+(431, 9, '顺', 'shùn', 'along, in the same direction as; obey; suitable'),
+(432, 9, '待', 'dài', 'treat; entertain; wait for   [dāi] (=呆) stay'),
+(433, 9, '局', 'jú', 'department; situation; 邮局 yóujú post office'),
+(434, 9, '护', 'hù', '(保护 bǎohù) guard, protect; 护士 hùshi nurse'),
+(435, 9, '拜', 'bài', 'honor, worship; 礼拜 lǐbài week   [bái] 拜拜 bye-bye'),
+(436, 9, '签', 'qiān', '签订 conclude & sign (treaty, etc); 签名 signature'),
+(437, 9, '证', 'zhèng', 'prove; 图书证 library card'),
+(438, 9, '领', 'lǐng', 'to lead; collar; 领导, 领袖 leader; 本领 ability'),
+(439, 9, '址', 'zhǐ', '地址 dìzhǐ address'),
+(440, 9, '墨', 'mò', '(墨水 mòshuǐ) ink; 墨子 Mòzǐ (a philosopher)'),
+(441, 9, '港', 'gǎng', 'harbor; 香港 Xiānggǎng Hong Kong'),
+(442, 9, '韩', 'Hán', '(surname); (old name for Korea)'),
+(443, 9, '斯', 'sī', '(thus; now used mostly for sound:) 法西斯 Fascist'),
+(444, 9, '蒂', 'dì', 'base of a fruit; 烟蒂 cigarette butt'),
+(445, 9, '夫', 'fū', 'man; 丈夫 husband; 工夫 skill; 大夫 dàifu doctor  [fú]');
 
 -- --------------------------------------------------------
 
@@ -312,7 +360,7 @@ CREATE TABLE `decks` (
   `id` int(10) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `decks`
@@ -325,7 +373,9 @@ INSERT INTO `decks` (`id`, `name`) VALUES
 (4, 'Lesson 4'),
 (5, 'Lesson 5'),
 (6, 'Lesson 6'),
-(7, 'Lesson 7');
+(7, 'Lesson 7'),
+(8, 'Lesson 8'),
+(9, 'Lesson 9');
 
 -- --------------------------------------------------------
 
@@ -349,7 +399,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`session_id`, `session_start`, `session_last_activity`, `session_ip_address`, `session_user_agent`, `session_data`) VALUES
-('15a1f2511f803f7675efdb128a301df4', 1195894618, 1195966289, '127.0.0.1', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv', 'a:2:{s:21:"current_answer_rating";i:1;s:11:"card_number";i:35;}');
+('33468e8edf53bdbb3eb317738bb9fdb2', 1197152246, 1197188481, '127.0.0.1', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv', 'a:4:{s:21:"current_answer_rating";i:0;s:11:"card_number";i:17;s:10:"learn_deck";i:7;s:7:"flipped";b:1;}');
 
 -- --------------------------------------------------------
 
@@ -359,7 +409,7 @@ INSERT INTO `sessions` (`session_id`, `session_start`, `session_last_activity`, 
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(100) NOT NULL auto_increment,
+  `id` int(10) NOT NULL auto_increment,
   `username` varchar(100) NOT NULL,
   `key` varchar(100) NOT NULL,
   `value` varchar(100) NOT NULL,
@@ -383,9 +433,10 @@ CREATE TABLE `user_progress` (
   `id` int(100) NOT NULL auto_increment,
   `card_id` int(100) NOT NULL,
   `flipped` tinyint(1) NOT NULL default '0',
-  `answer_rating` float NOT NULL,
-  `date_answered` int(10) NOT NULL,
-  `time_to_answer` int(10) NOT NULL,
+  `answer_rating` float NOT NULL default '0',
+  `next_repetition_date` int(10) NOT NULL,
+  `interval` int(10) NOT NULL default '0',
+  `repetitions_to_memorize` int(10) NOT NULL default '0',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
